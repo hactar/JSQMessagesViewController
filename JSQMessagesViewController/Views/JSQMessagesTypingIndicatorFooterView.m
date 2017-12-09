5b5975e8fcdc7f06fa36fe9cb22ce140f5693677
Hactar <patrick@subzero.eu>
2017-12-09 16:06:20 +0100

    - light instead of dark dots.

--- JSQMessagesViewController/Views/JSQMessagesTypingIndicatorFooterView.m
+++ JSQMessagesViewController/Views/JSQMessagesTypingIndicatorFooterView.m
@@ -103,8 +103,8 @@ const CGFloat kJSQMessagesTypingIndicatorFooterViewHeight = 46.0f;
     
     [self setNeedsUpdateConstraints];
     
-    self.typingView.dotsColor = ellipsisColor;
-    self.typingView.animateToColor = [ellipsisColor jsq_colorByDarkeningColorWithValue:0.2f];
+    self.typingView.dotsColor = [UIColor lightTextColor];
+    self.typingView.animateToColor = [UIColor whiteColor];
     self.typingView.animated = animated;
     self.typingView.animationDuration = 1.33;
     
