edc3d734afb0011cbac34f574cf8720652432117
PW <patrick@subzero.eu>
2019-04-16 23:27:38 +0200

    changing share to actionsheet

--- JSQMessagesViewController/Views/JSQMessagesCollectionViewCellIncoming.m
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewCellIncoming.m
@@ -30,13 +30,36 @@
         self.shareButton.imageView.contentMode = UIViewContentModeScaleAspectFit;
     }
     
-- (IBAction)shareButtonTapped:(UIButton *)sender {
+- (void)actuallyShare:(UIButton *)sender {
     NSLog(@"Going to share %@", self.shareURL);
     UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[self.shareURL] applicationActivities:nil];
     activityViewController.popoverPresentationController.sourceView = sender;
     activityViewController.popoverPresentationController.sourceRect = sender.bounds;
     
     [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:activityViewController animated:YES completion:nil];
+}
+
+- (IBAction)shareButtonTapped:(UIButton *)sender {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Weitere Optionen" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    alertController.popoverPresentationController.sourceView = sender;
+    alertController.popoverPresentationController.sourceRect = sender.bounds;
+    
+    UIAlertAction *action1 = [UIAlertAction actionWithTitle:@"Antwort mit anderen teilen" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self actuallyShare:sender];
+    }];
+    
+    UIAlertAction *action2 = [UIAlertAction actionWithTitle:@"Feedback geben" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"Open Feedback interface...");
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Abbrechen" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        // do nothing
+    }];
+    [alertController addAction:action1];
+    [alertController addAction:action2];
+    [alertController addAction:cancelAction];
+    
+    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alertController animated:YES completion:nil];
 }
     
     @end
